d24ac402c7eeec1f61b55498ac17425a0b8a7a98
Xie hongwei <xiehw@hodorware.com>
2019-07-31 11:13:00 +0800

    First Actions

---
+++ src/actions/shared.js
@@ -0,0 +1,15 @@
+import { getInitialData } from '../utils/api';
+
+import { receiveTweets } from '../actions/tweets';
+import { receiveUsers } from '../actions/users';
+
+
+export function handleInitialData () {
+    return (dispatch) => {
+        return getInitialData()
+            .then(({ users, tweets }) => {
+                dispatch(receiveUsers(users));
+                dispatch(receiveTweets(tweets));
+            })
+    }
+}
