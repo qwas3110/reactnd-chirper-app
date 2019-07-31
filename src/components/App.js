ee0551fe5ba29d8ed11624923f838fa1d491411d
qwas3110 <283994917@qq.com>
2019-07-31 14:43:29 +0800

    Dashboard

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 
 import { handleInitialData } from '../actions/shared';
+import Dashboard from "./Dashboard";
 
 
 class App extends Component {
@@ -15,12 +16,27 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
+        {
+          this.props.loading === true
+            ? null
+            : <Dashboard/>
+        }
+
       </div>
     )
   }
 }
 
-export default connect(
 
+// 用户未验证成功前，显示loading
+function mapStateToProps ({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+
+
+export default connect(
+    mapStateToProps
 )(App);
